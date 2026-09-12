778d33b508710a73d9f87bbccf6f50cc6e8f764c
Christian Noll <christiannoll@gmx.net>
2026-09-12 12:51:03 +0200

    Assign Crouch_Shapecast

--- controllers/scripts/fps_controller.gd
+++ controllers/scripts/fps_controller.gd
@@ -8,6 +8,7 @@ extends CharacterBody3D
 @export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
 @export var CAMERA_CONTROLLER : Camera3D
 @export var ANIMATIONPLAYER: AnimationPlayer
+@export var CROUCH_SHAPECAST: Node3D
 
 var _mouse_input : bool = false
 var _rotation_input : float
