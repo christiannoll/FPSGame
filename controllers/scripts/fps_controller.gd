0effe7ebc25e4d181c20b27814a1798bc3130aea
Christian Noll <christiannoll@gmx.net>
2026-09-13 12:34:34 +0200

    Permit jumping when crouching

--- controllers/scripts/fps_controller.gd
+++ controllers/scripts/fps_controller.gd
@@ -33,7 +33,7 @@ func _input(event):
 	
 	if event.is_action_pressed("exit"):
 		get_tree().quit()
-	if event.is_action_pressed("crouch"):
+	if event.is_action_pressed("crouch") and is_on_floor():
 		toggle_crouch()
 		
 func _update_camera(delta):
@@ -71,7 +71,7 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and _is_crouching == false:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
