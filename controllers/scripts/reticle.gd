750913834273a36ed3a1f3cbf58da3dd45fd1592
Christian Noll <christiannoll@gmx.net>
2026-09-22 19:17:39 +0200

    Added some variables in reticle script

--- controllers/scripts/reticle.gd
+++ controllers/scripts/reticle.gd
@@ -1,5 +1,9 @@
 extends CenterContainer
 
+@export var RETICLE_LINES: Array[Line2D]
+@export var PLAYER_CONTROLLER: CharacterBody3D
+@export var RETICLE_SPEED: float = 0.25
+@export var RETICLE_DISTANCE: float = 2.0
 @export var DOT_RADIUS: float = 1.0
 @export var DOT_COLOR: Color = Color.WHITE
 
